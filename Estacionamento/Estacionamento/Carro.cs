ec3ba55fae87430c03984fc0e703547658196d57
Willian Johan <johan_willian@hotmail.com>
2021-09-30 11:34:35 -0300

    Implementação - Estrutura de carro

---
+++ Estacionamento/Estacionamento/Carro.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Estacionamento
+{
+    public struct Carro
+    {
+        public string Placa { get; private set; }
+        public string Modelo { get; private set; }
+        public string Cor { get; private set; }
+
+        public Carro(string placa, string modelo, string cor)
+        {
+            Placa = placa;
+            Modelo = modelo;
+            Cor = cor;
+        }
+
+
+    }
+}
