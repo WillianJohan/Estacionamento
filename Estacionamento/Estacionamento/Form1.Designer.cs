9ccee7c3885b4b63af919908e02c9b6cab68d6da
Willian Johan <johan_willian@hotmail.com>
2021-09-30 09:42:05 -0300

    Teste - Implementação teste inicial utilizando DataGrid
    Testando a interface.
    
    Utilizando DataGrid para inserir e remover valores

---
+++ Estacionamento/Estacionamento/Form1.Designer.cs
@@ -0,0 +1,134 @@
+﻿
+namespace Estacionamento
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnSaida = new System.Windows.Forms.Button();
+            this.dataGrid = new System.Windows.Forms.DataGridView();
+            this.Placa = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.ModeloVeiculo = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Cor = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.HoraEntrada = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.btnAddVeicle = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGrid)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnSaida
+            // 
+            this.btnSaida.Location = new System.Drawing.Point(572, 590);
+            this.btnSaida.Name = "btnSaida";
+            this.btnSaida.Size = new System.Drawing.Size(125, 39);
+            this.btnSaida.TabIndex = 0;
+            this.btnSaida.Text = "Saída";
+            this.btnSaida.UseVisualStyleBackColor = true;
+            this.btnSaida.Click += new System.EventHandler(this.btnSaida_Click);
+            // 
+            // dataGrid
+            // 
+            this.dataGrid.AllowUserToAddRows = false;
+            this.dataGrid.AllowUserToDeleteRows = false;
+            this.dataGrid.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGrid.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Placa,
+            this.ModeloVeiculo,
+            this.Cor,
+            this.HoraEntrada});
+            this.dataGrid.Location = new System.Drawing.Point(12, 385);
+            this.dataGrid.MultiSelect = false;
+            this.dataGrid.Name = "dataGrid";
+            this.dataGrid.RowTemplate.Height = 25;
+            this.dataGrid.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dataGrid.Size = new System.Drawing.Size(543, 244);
+            this.dataGrid.TabIndex = 1;
+            // 
+            // Placa
+            // 
+            this.Placa.HeaderText = "Placa";
+            this.Placa.Name = "Placa";
+            this.Placa.ReadOnly = true;
+            this.Placa.Resizable = System.Windows.Forms.DataGridViewTriState.False;
+            // 
+            // ModeloVeiculo
+            // 
+            this.ModeloVeiculo.HeaderText = "Modelo do veículo";
+            this.ModeloVeiculo.Name = "ModeloVeiculo";
+            this.ModeloVeiculo.ReadOnly = true;
+            this.ModeloVeiculo.Width = 150;
+            // 
+            // Cor
+            // 
+            this.Cor.HeaderText = "Cor";
+            this.Cor.Name = "Cor";
+            this.Cor.ReadOnly = true;
+            // 
+            // HoraEntrada
+            // 
+            this.HoraEntrada.HeaderText = "Hora de Entrada";
+            this.HoraEntrada.Name = "HoraEntrada";
+            this.HoraEntrada.ReadOnly = true;
+            this.HoraEntrada.Width = 150;
+            // 
+            // btnAddVeicle
+            // 
+            this.btnAddVeicle.Location = new System.Drawing.Point(253, 141);
+            this.btnAddVeicle.Name = "btnAddVeicle";
+            this.btnAddVeicle.Size = new System.Drawing.Size(202, 66);
+            this.btnAddVeicle.TabIndex = 2;
+            this.btnAddVeicle.Text = "Adicionar Veículo";
+            this.btnAddVeicle.UseVisualStyleBackColor = true;
+            this.btnAddVeicle.Click += new System.EventHandler(this.btnAddVeicle_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(709, 645);
+            this.Controls.Add(this.btnAddVeicle);
+            this.Controls.Add(this.dataGrid);
+            this.Controls.Add(this.btnSaida);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGrid)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnSaida;
+        private System.Windows.Forms.DataGridView dataGrid;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Placa;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ModeloVeiculo;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Cor;
+        private System.Windows.Forms.DataGridViewTextBoxColumn HoraEntrada;
+        private System.Windows.Forms.Button btnAddVeicle;
+    }
+}
+
---
+++ Estacionamento/Estacionamento/Form1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Estacionamento
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSaida_Click(object sender, EventArgs e)
+        {
+            //Garante que tenha algum veículo selecionado na lista
+            if (dataGrid.SelectedRows.Count == 0)
+                return;
+
+            int rowIndex = dataGrid.CurrentCell.RowIndex;
+            dataGrid.Rows.RemoveAt(rowIndex);
+        }
+
+        private void btnAddVeicle_Click(object sender, EventArgs e)
+        {
+            //Codigo fonte que usei para estudar a dataGrid
+            //http://www.macoratti.net/08/08/c_pdgv1.htm
+
+            //define um array de strings com nCOlunas
+
+            string[] linhaDados = new string[4];
+            linhaDados[0] = "Colum 1";
+            linhaDados[1] = "Colum 2";
+            linhaDados[2] = "Colum 3";
+            linhaDados[3] = "Colum 4";
+
+            //dataGrid.Rows.Add(new DataGridViewRow());
+            dataGrid.Rows.Add(linhaDados);
+        }
+    }
+}
