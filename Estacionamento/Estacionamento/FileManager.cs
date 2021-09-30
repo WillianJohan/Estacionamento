58ef5824c1fa4314b76ec6ec6fa10c31552b4519
Willian Johan <johan_willian@hotmail.com>
2021-09-30 11:52:22 -0300

    Implementação - Interface e classe FileManager

---
+++ Estacionamento/Estacionamento/FileManager.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Estacionamento
+{
+    public class FileManager : IFileManager
+    {
+        void IFileManager.CreateFile(string path) => File.Create(path);
+
+        public string LoadFile(string path)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void AddCar(string path, Carro carro)
+        {
+            Stream output = File.Open(path, FileMode.Append);
+            //StreamWriter writer = new StreamWriter(output);
+            //foreach (int item in unsorted) writer.WriteLine(item);
+            //writer.Close();
+            output.Close();
+        }
+
+        public void RemoveCar(string path, Carro carro)
+        {
+            throw new NotImplementedException();
+        }
+        
+    }
+}
---
+++ Estacionamento/Estacionamento/IFileManager.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Estacionamento
+{
+    public interface IFileManager
+    {
+        protected void CreateFile(string path);
+        string LoadFile(string path); //TODO: Criar uma estrutura de parquimetro para tratar o texto e retornar as informações
+        void AddCar(string path, Carro carro);
+        void RemoveCar(string path, Carro carro);
+        
+    }
+}
