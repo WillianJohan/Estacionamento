1a2a4b8646d1e5bbdb44b1786cd1dde6cc556ede
Willian Johan <johan_willian@hotmail.com>
2021-10-07 11:12:21 -0300

    Create ParkingSpace.cs

---
+++ Estacionamento/Estacionamento/ParkingSpace.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Estacionamento
+{
+    public class ParkingSpace
+    {
+        public Carro carro;
+        public DateTime Entrada;
+
+        public ParkingSpace(Carro carro, DateTime entrada)
+        {
+            this.carro = carro;
+            Entrada = entrada;
+        }
+    }
+}
