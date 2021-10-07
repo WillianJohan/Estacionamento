d3723b21b9f3688d5c4d59dee5e6ea6b4fafcf15
Willian Johan <johan_willian@hotmail.com>
2021-10-07 11:12:26 -0300

    Create Parking.cs

---
+++ Estacionamento/Estacionamento/Parking.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Estacionamento
+{
+    public class Parking
+    {
+        public List<ParkingSpace> Vagas;
+
+        public Parking(List<ParkingSpace> Vagas)
+        {
+            this.Vagas = Vagas;
+        }
+
+        public Parking()
+        {
+            this.Vagas = new List<ParkingSpace>();
+        }
+    }
+}
