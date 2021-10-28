c6c889dce87b81b447a1058a8d0e900c50d5fc00
Willian Johan <johan_willian@hotmail.com>
2021-10-28 09:30:21 -0300

    imp - Implementação do login

--- Estacionamento-WPF/Estacionamento-WPF/View/LoginWindow.xaml.cs
+++ Estacionamento-WPF/Estacionamento-WPF/View/LoginWindow.xaml.cs
@@ -22,6 +22,30 @@ namespace Estacionamento_WPF.View
         public LoginWindow()
         {
             InitializeComponent();
+            passInput.Focus();
+        }
+
+        private void passInput_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key != Key.Enter)
+                return;
+
+            if (!IsValidPassword())
+            {
+                MessageBox.Show("Invalid Password");
+                passInput.Clear();
+                return;
+            }
+
+            MainWindow mw = new MainWindow();
+            mw.Show();
+            Close();
+        }
+
+        bool IsValidPassword()
+        {
+            string passText = passInput.Password;
+            return passText.Equals("estacionamento");
         }
     }
 }
