26be08f40b1c36ac2a1954855c88fe68a2afb8eb
Willian Johan <johan_willian@hotmail.com>
2021-11-11 12:51:16 -0300

    Update

---
+++ Estacionamento-WPF/Estacionamento-WPF/Model/ParkingRecord.cs
@@ -0,0 +1,20 @@
+﻿using SQLite;
+using System;
+
+namespace Estacionamento_WPF.Model
+{
+    [Table("ParkingRecord")]
+    public class ParkingRecord
+    {
+        [PrimaryKey, AutoIncrement]
+        public int ID { get; set; }
+
+        [NotNull, MaxLength(8)]
+        public string Plate { get; set; }
+
+        public string Model { get; set; }
+
+        public DateTime EntryTime { get; set; }
+        public DateTime DepartureTime { get; set; }
+    }
+}
