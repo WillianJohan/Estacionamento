70d4871a3cc69ecb5ab1b007482f0b926294b86a
Willian Johan <johan_willian@hotmail.com>
2021-11-13 13:57:48 -0300

    Create RemoveParkingRecordItemCommand.cs

---
+++ Estacionamento-WPF/Estacionamento-WPF/ViewModel/Commands/RemoveParkingRecordItemCommand.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Estacionamento_WPF.ViewModel.Commands
+{
+    public class RemoveParkingRecordItemCommand : CommandBase
+    {
+
+        public MainWindowVM MyViewModel;
+
+        public RemoveParkingRecordItemCommand(MainWindowVM MyViewModel)
+            => this.MyViewModel = MyViewModel;
+
+        public override void Execute(object parameter)
+        {
+            MyViewModel.RemoveParkingItemRecord();
+        }
+    }
+}
