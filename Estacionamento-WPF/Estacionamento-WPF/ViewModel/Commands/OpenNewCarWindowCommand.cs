0923dd579b5d5a7a76a743537de2e8b296670272
Willian Johan <johan_willian@hotmail.com>
2021-11-13 13:57:52 -0300

    Create OpenNewCarWindowCommand.cs

---
+++ Estacionamento-WPF/Estacionamento-WPF/ViewModel/Commands/OpenNewCarWindowCommand.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Estacionamento_WPF.ViewModel.Commands
+{
+    public class OpenNewCarWindowCommand : CommandBase
+    {
+        public MainWindowVM MyViewModel;
+
+        public OpenNewCarWindowCommand(MainWindowVM MyViewModel)
+            => this.MyViewModel = MyViewModel;
+
+        public override void Execute(object parameter)
+        {
+            MyViewModel.OpenWindowCommand_AddNewCar();
+        }
+    }
+}
