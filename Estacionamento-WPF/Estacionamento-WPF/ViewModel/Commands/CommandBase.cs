d522b61704232b93ca3bdfad8ab24d646ff4fb52
Willian Johan <johan_willian@hotmail.com>
2021-11-13 10:54:16 -0300

    imp - Command Base

---
+++ Estacionamento-WPF/Estacionamento-WPF/ViewModel/Commands/CommandBase.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.ComponentModel;
+using System.Windows.Input;
+
+namespace Estacionamento_WPF.ViewModel.Commands
+{
+    public abstract class CommandBase : ICommand
+    {
+        public abstract void Execute(object parameter);
+        public virtual bool CanExecute(object parameter) => true;
+
+
+        public event EventHandler CanExecuteChanged;
+        protected void OnCanExecuteChanged(object sender, PropertyChangedEventArgs e)
+            => CanExecuteChanged?.Invoke(this, new EventArgs());
+    }
+}
