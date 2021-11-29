9260a9859ed497a3a9dcbb86e5b6963875f933f9
Willian Johan <johan_willian@hotmail.com>
2021-11-29 17:23:09 -0300

    Update e command do updateInfoVM implementado

---
+++ Estacionamento-WPF/Estacionamento-WPF/ViewModel/Commands/UpdateInfoCommand.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Estacionamento_WPF.ViewModel.Commands
+{
+    public class UpdateInfoCommand : CommandBase
+    {
+        UpdateInfoVM vm;
+
+        public UpdateInfoCommand(UpdateInfoVM vm) => this.vm = vm;
+
+        public override bool CanExecute(object parameter)
+        {
+            return string.IsNullOrEmpty(vm.Plate);
+        }
+
+        public override void Execute(object parameter)
+        {
+            vm.Save();
+        }
+    }
+}
---
+++ Estacionamento-WPF/Estacionamento-WPF/ViewModel/UpdateInfoVM.cs
@@ -0,0 +1,62 @@
+﻿using Estacionamento_WPF.Model;
+using System.Windows;
+
+namespace Estacionamento_WPF.ViewModel
+{
+    public class UpdateInfoVM : ViewModelBase
+    {
+        public static ParkingRecord ItemToUpdate {get;set;}
+
+        string plate;
+        public string Plate
+        {
+            get => plate;
+            set
+            {
+                plate = value;
+                OnPropertyChanged(nameof(Plate));
+            }
+        }
+
+        string model;
+        public string Model
+        {
+            get => model;
+            set
+            {
+                model = value;
+                OnPropertyChanged(nameof(Model));
+            }
+        }
+
+        public UpdateInfoVM()
+        {
+            if(!ItemToUpdate.CanSaveInDatabase())
+            {
+                MessageBox.Show("Informações Nulas");
+                View.UpdateParkingRecordWindow.Instance.Close();
+            }
+        }
+
+        public void Save()
+        {
+            ParkingRecord updatedPR = ItemToUpdate;
+            updatedPR.Model = model;
+            updatedPR.Plate = plate;
+
+
+            bool operationSucess = Database.ParkingOperations.Update(ItemToUpdate, updatedPR);
+
+
+            if (operationSucess) //Se for ok, fecha a janela
+                View.UpdateParkingRecordWindow.Instance.Close();
+            else
+            {
+                MessageBox.Show("Não foi possivel salvar");
+            }
+
+
+        }
+
+    }
+}
