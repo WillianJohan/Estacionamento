600ae6260764c1830fbb14733b41484b705dc9bc
Willian Johan <johan_willian@hotmail.com>
2021-11-29 17:06:41 -0300

    imp - adicionado referencia estatica das janelas

--- Estacionamento-WPF/Estacionamento-WPF/View/UpdateParkingRecordWindow.xaml.cs
+++ Estacionamento-WPF/Estacionamento-WPF/View/UpdateParkingRecordWindow.xaml.cs
@@ -1,27 +1,17 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
+﻿using System.Windows;
+
 
 namespace Estacionamento_WPF.View
 {
-    /// <summary>
-    /// Lógica interna para UpdateParkingRecordWindow.xaml
-    /// </summary>
+
     public partial class UpdateParkingRecordWindow : Window
     {
+        public static UpdateParkingRecordWindow Instance { get; private set; }
+
         public UpdateParkingRecordWindow()
         {
             InitializeComponent();
+            Instance = this;
         }
     }
 }
