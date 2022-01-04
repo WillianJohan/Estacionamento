ed40bf1b5a4aa06575218c4a03a06100abfd524b
Willian Johan <johan_willian@hotmail.com>
2022-01-04 17:45:47 -0300

    Tela de login refatorada

--- Estacionamento-WPF/Estacionamento-WPF/View/LoginWindow.xaml.cs
+++ Estacionamento-WPF/Estacionamento-WPF/View/LoginWindow.xaml.cs
@@ -12,30 +12,30 @@ namespace Estacionamento_WPF.View
         {
             InitializeComponent();
             Instance = this;
-            passInput.Focus();
+            //passInput.Focus();
         }
 
-        private void passInput_KeyDown(object sender, KeyEventArgs e)
-        {
-            if (e.Key != Key.Enter)
-                return;
-
-            if (!IsValidPassword())
-            {
-                MessageBox.Show("Invalid Password");
-                passInput.Clear();
-                return;
-            }
-
-            MainWindow mw = new MainWindow();
-            mw.Show();
-            Close();
-        }
-
-        bool IsValidPassword()
-        {
-            string passText = passInput.Password;
-            return passText.Equals("estacionamento");
-        }
+        //private void passInput_KeyDown(object sender, KeyEventArgs e)
+        //{
+        //    if (e.Key != Key.Enter)
+        //        return;
+        //
+        //    if (!IsValidPassword())
+        //    {
+        //        MessageBox.Show("Invalid Password");
+        //        passInput.Clear();
+        //        return;
+        //    }
+        //
+        //    MainWindow mw = new MainWindow();
+        //    mw.Show();
+        //    Close();
+        //}
+        //
+        //bool IsValidPassword()
+        //{
+        //    string passText = passInput.Password;
+        //    return passText.Equals("estacionamento");
+        //}
     }
 }
